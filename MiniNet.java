fe0fecb70ccf3b61c1b05dbee9aa08c5ddd0464e
rmit-s3647369-He-Yidian <31241532+rmit-s3647369-He-Yidian@users.noreply.github.com>
2018-03-22 23:48:14 +1100

    Add files via upload

---
+++ MiniNet.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package MiniNet;
+
+/**
+ * @author 13482
+ *
+ */
+public class MiniNet {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	}
+
+}
